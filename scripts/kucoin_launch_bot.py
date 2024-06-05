272cdc8cc21a79cb7c24b20a435c04ca3314b161
paulnheera <paul.nheera@gmail.com>
2024-06-05 14:50:45 +0200

    Minor updates to some code in kucoin_launch_bot.

--- scripts/kucoin_launch_bot.py
+++ scripts/kucoin_launch_bot.py
@@ -55,7 +55,7 @@ STOP_LOSS = 0.5
 
 async def main(symbol):
     
-    print(f'{current_time()} | Start process...')
+    print(f'{current_time()} | INFO | Start process...')
     print(f'   Symbol: {SYMBOL}')
     print(f'   Amount: {AMOUNT}')
     print(f'   Take Profit: {TAKE_PROFIT}')
@@ -67,12 +67,12 @@ async def main(symbol):
             nonlocal EXECUTED
                         
             if not EXECUTED:
-                print('Not Executed!')
+                print(f'{current_time()} | INFO | No trades executed.')
                 
                 # Get besk ask price
                 try:
                     ask_price =  my_kucoin.get_ask(symbol)
-                    print(f'{current_time()} | Ask Price: {ask_price}')
+                    print(f'{current_time()} | INFO | Ask Price: {ask_price}')
                 except Exception as e:
                     print(f'ERROR: {e}')
                 
@@ -83,7 +83,7 @@ async def main(symbol):
                 q = float(result.get('baseIncrement'))      # quantity precision
                 q_min = float(result.get('baseMinSize'))    # minimum quantity
                 
-                time.sleep(1) # Sleep for one second.
+                time.sleep(1) # Sleep for one second, to manage API rate limits.
                 
                 #*********************************************************************************************************
                 # Check Existing Balance
@@ -96,7 +96,7 @@ async def main(symbol):
                     price = float(buys_df['price'].apply(pd.to_numeric).mean())
                     stop_price = price * (1+0.1) # 10% profit Limit Order #TODO:
                     stop_price = round(round(stop_price / p) * p, int(abs(math.log10(p))))
-                    print(f'{current_time()} | Existing position of {size}, bought at {price}.')
+                    print(f'{current_time()} | INFO |  Existing position of {size}, bought at {price}.')
                     
                     if q==1:
                         size=int(size)
@@ -104,12 +104,12 @@ async def main(symbol):
                     # Set Exit Price for Current Position
                     try:
                         limit_order = tradeClient.create_limit_order(SYMBOL, 'sell', str(size), str(stop_price))
-                        print(f'{current_time()} | Limit order set for existing position.')
+                        print(f'{current_time()} | INFO | Limit order set for existing position.')
                     except Exception as e:
-                        print(f'{current_time()} | Failed to place limit order for existing position')
+                        print(f'{current_time()} | INFO | Failed to place limit order for existing position')
                         print(f'ERROR: {e}')
                 else:
-                    print(f'{current_time()} | No existing position (No orders filled.)')
+                    print(f'{current_time()} | INFO | No existing position (No orders filled.)')
                 ##TODO: Need an else statement here. Either we exit an exist pre-market order or we enter into one.
                 ##TODO: Also if we are entering into one, we need to cancel any non filled pre-market orders.
                 
@@ -123,50 +123,55 @@ async def main(symbol):
                 # Place buy order
                 try:
                     order_id = tradeClient.create_limit_order(SYMBOL, 'buy', str(qty), ask_price)
-                    print(f'{current_time()} | Buy limit order placed for {qty} {SYMBOL} at {ask_price}.')
+                    print(f'{current_time()} | INFO | Buy limit order placed for {qty} {SYMBOL} at {ask_price}.')
                 except Exception as e:
-                    print(f'{current_time()} | Failed to place buy limit order!')
+                    print(f'{current_time()} | INFO | Failed to place buy limit order!')
                     print(f'ERROR: {e}')
                     
                 
                 # Place take profit
                 take_profit = float(ask_price) * (1+TAKE_PROFIT)
-                take_profit = round(round(take_profit / p) * p, int(-math.log10(p)))
+                take_profit = round(round(take_profit / p) * p, int(abs(math.log10(p))))
                 try:
                     tp_order = tradeClient.create_limit_order(SYMBOL,
                                                               side='sell',
                                                               size=str(qty),
                                                               price=str(take_profit)
                                                               )
-                    print(f'{current_time()} | Take profit set at {take_profit}.')
+                    print(f'{current_time()} | INFO | Take profit set at {take_profit}.')
                 except Exception as e:
-                    print(f'{current_time()} | Failed to place take profit limit stop order!')
+                    print(f'{current_time()} | INFO | Failed to place take profit limit stop order!')
                     print(f'ERROR: {e}')
 
                 # Place stop loss
                 stop_loss = float(ask_price) * (1-STOP_LOSS)
-                stop_loss = round(round(stop_loss / p) * p, int(-math.log10(p)))
+                stop_loss = round(round(stop_loss / p) * p, int(abs(math.log10(p))))
                 try:
                     sl_order = tradeClient.create_limit_stop_order(SYMBOL,
                                                                     side='sell',
                                                                     size=str(qty),
                                                                     price=str(stop_loss),
                                                                     stopPrice=str(stop_loss))
-                    print(f'{current_time()} | Stop loss set at {stop_loss}.')
+                    print(f'{current_time()} | INFO | Stop loss set at {stop_loss}.')
                 except Exception as e:
-                    print(f'{current_time()} | Failed to place stop loss limit stop order!')
+                    print(f'{current_time()} | INFO | Failed to place stop loss limit stop order!')
                     print(f'ERROR: {e}')
                 
                 EXECUTED = True
+                
+            ## Manage Trade
+            # Ideas:
+            # If the sell velocity is increasing, exit the 
             
             
             ## SAVE DATA!
             today = datetime.now().date()
             if msg['topic'] == f'/market/match:{symbol}':
                 data = msg['data']
-                data_str = json.dumps(data)
+                data_str = json.dumps(data) # converts a subset of python objects into a json string.
                 async with aiofiles.open(f"{symbol} - trades - {today}.txt", mode = "a") as f:
                     await f.write(data_str + "\n") # await just tells python: "while doing this job is running, if there is nothing to do, feel free to go an do something else."
+            
             elif msg['topic'] == f'/spotMarket/level2Depth5:{symbol}':
                 data = msg['data']
                 data_str = json.dumps(data)
